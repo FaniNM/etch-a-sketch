bdf67575fed26540d6bcfffbcc1bdf82ad121a8b
faniNM <fm399@cornell.edu>
2018-03-29 16:30:28 -0500

    lab6

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,26 +1,53 @@
-#define SENSORPINA A0 // x axis
- //TODO: define other sensor inputs
 
-unsigned long targetTime=0;
-const unsigned long interval=2500; //TODO: How fast should we read
-void setup(){
-// TODO: begin the serial connection with a baudrate of 115200
+const int VRxPin= A0; //VRx pin connected to arduino pin A0
+const int VRyPin= A1; //VRy pin connected to arduino in A1
+const int SwButtonPin= 8; //SW pin connected to arduino pin D8
+const int R = 2;
+int pressed= 0; //this variable will determine whether joystick has been pressed down (selected)
+int x= 0;//this variable will hold the X-coordinate value
+int y= 0; //this variable will hold the Y-coordinate value
+int reset = 0; // this variable will hold the reset value
+String string_x;
+String string_y;
+
+unsigned long targetTime = 0;
+const unsigned long interval = 1000;
+
+
+void setup() {
+  pinMode(SwButtonPin, INPUT);//sets the SW switch as input
+  pinMode(VRxPin, INPUT);
+  pinMode(VRyPin, INPUT);
+  pinMode(R, INPUT);
+  Serial.begin(115200);//sets the baud rate
 }
 
 
-void loop(){
-	if(millis()>=targetTime){
-		targetTime= millis()+interval;
-		Serial.println(analogRead(SENSORPINA));
+void loop() {
 
-		 //TODO: Add other sensor read outs
-     //TODO: convert values into a string https://www.arduino.cc/en/Tutorial/StringConstructors
-		 //TODO: combine them into a string that can be understood by server.js
-		 //TODO: send the string over serial
+  
+  if (millis() >= targetTime) {
 
+    targetTime = millis() + interval;
 
-	}
-	// TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
-  // TODO: write the reset message(see server.js) to the serial port
+    reset = digitalRead(R);
+
+    if (reset == LOW) { //If button is pressed, reset
+      pressed= digitalRead(SwButtonPin);//reads whether joystick has been pressed down (selected) or not
+      x= analogRead(VRxPin);//reads the X-coordinate value
+      y= analogRead(VRyPin);//reads the Y-coordinate value
+      string_x = String(x);
+      string_y =String(y);
+      Serial.println(string_x + "," + string_y);
+    } 
+    
+    else {
+        Serial.println(String("rst"));
+        x = 0;
+        y = 0;
+      }
+    }
 
 }
+
+
